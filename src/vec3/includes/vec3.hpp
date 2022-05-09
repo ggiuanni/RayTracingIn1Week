3b8b78509f400b2b9beaf43ff301c87af4334c06
ggiuanni <giovanni.csn@gmail.com>
2022-05-09 22:36:23 +0200

    vec3 finish

--- src/vec3/includes/vec3.hpp
+++ src/vec3/includes/vec3.hpp
@@ -12,44 +12,19 @@ public:
     vec3() : e{0, 0, 0} {}
     vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
-    double x() const { return e[0]; }
-    double y() const { return e[1]; }
-    double z() const { return e[2]; }
+    double x() const;
+    double y() const;
+    double z() const;
+    double length() const;
+    double length_squared() const;
 
-    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-    double operator[](int i) const { return e[i]; }
-    double &operator[](int i) { return e[i]; }
+    vec3 operator-() const;
+    double operator[](int i) const;
+    double &operator[](int i);
 
-    vec3 &operator+=(const vec3 &v)
-    {
-        e[0] += v.e[0];
-        e[1] += v.e[1];
-        e[2] += v.e[2];
-        return *this;
-    }
-
-    vec3 &operator*=(const double t)
-    {
-        e[0] *= t;
-        e[1] *= t;
-        e[2] *= t;
-        return *this;
-    }
-
-    vec3 &operator/=(const double t)
-    {
-        return *this *= 1 / t;
-    }
-
-    double length() const
-    {
-        return sqrt(length_squared());
-    }
-
-    double length_squared() const
-    {
-        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
-    }
+    vec3 &operator+=(const vec3 &v);
+    vec3 &operator*=(const double t);
+    vec3 &operator/=(const double t);
 
 public:
     double e[3];
@@ -58,5 +33,18 @@ public:
 // Type aliases for vec3
 using point3 = vec3; // 3D point
 using color = vec3;  // RGB color
+
+// vec3 Utility Functions
+
+inline std::ostream &operator<<(std::ostream &out, const vec3 &v);
+inline vec3 operator+(const vec3 &u, const vec3 &v);
+inline vec3 operator-(const vec3 &u, const vec3 &v);
+inline vec3 operator*(const vec3 &u, const vec3 &v);
+inline vec3 operator*(double t, const vec3 &v);
+inline vec3 operator*(const vec3 &v, double t);
+inline vec3 operator/(vec3 v, double t);
+inline double dot(const vec3 &u, const vec3 &v);
+inline vec3 cross(const vec3 &u, const vec3 &v);
+inline vec3 unit_vector(vec3 v);
 
 #endif
