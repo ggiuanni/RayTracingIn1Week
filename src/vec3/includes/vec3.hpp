d375a97a4fd79a1f4ad3499ba509cf7b2010e5f7
ggiuanni <giovanni.csn@gmail.com>
2022-05-10 00:01:53 +0200

    ray

--- src/vec3/includes/vec3.hpp
+++ src/vec3/includes/vec3.hpp
@@ -6,10 +6,8 @@
 class vec3
 {
 public:
-    double e[3];
-
-    vec3() : e{0, 0, 0} {}
-    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+    vec3();
+    vec3(double e0, double e1, double e2);
 
     double x() const;
     double y() const;
@@ -24,6 +22,9 @@ public:
     vec3 &operator+=(const vec3 &v);
     vec3 &operator*=(const double t);
     vec3 &operator/=(const double t);
+
+public:
+    double e[3];
 };
 
 // Type aliases for vec3
@@ -31,15 +32,15 @@ using point3 = vec3; // 3D point
 using color = vec3;  // RGB color
 
 // vec3 Utility Functions
-inline std::ostream &operator<<(std::ostream &out, const vec3 &v);
-inline vec3 operator+(const vec3 &u, const vec3 &v);
-inline vec3 operator-(const vec3 &u, const vec3 &v);
-inline vec3 operator*(const vec3 &u, const vec3 &v);
-inline vec3 operator*(double t, const vec3 &v);
-inline vec3 operator*(const vec3 &v, double t);
-inline vec3 operator/(vec3 v, double t);
-inline double dot(const vec3 &u, const vec3 &v);
-inline vec3 cross(const vec3 &u, const vec3 &v);
-inline vec3 unit_vector(vec3 v);
+std::ostream &operator<<(std::ostream &out, const vec3 &v);
+vec3 operator+(const vec3 &u, const vec3 &v);
+vec3 operator-(const vec3 &u, const vec3 &v);
+vec3 operator*(const vec3 &u, const vec3 &v);
+vec3 operator*(double t, const vec3 &v);
+vec3 operator*(const vec3 &v, double t);
+vec3 operator/(vec3 v, double t);
+double dot(const vec3 &u, const vec3 &v);
+vec3 cross(const vec3 &u, const vec3 &v);
+vec3 unit_vector(vec3 v);
 
 #endif
